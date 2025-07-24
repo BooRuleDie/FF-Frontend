0863a299441aafb06a9a0915b7df92154677e77a
BooRuleDie <booruledie@gmail.com>
2025-07-24 10:21:29 +0300

    Revert "Reapply "Reapply "admin users"""
    This reverts commit 8e91e8bf0d8d5544c49891766f3dc6d99e545746.

--- src/hook.ts
+++ src/hook.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchUsers, fetchDevelopers, fetchAdmins } from "./api";
+import { fetchUsers, fetchDevelopers } from "./api";
 
 export const useGetUsers = () => {
     const { data: users, isLoading } = useQuery({
@@ -15,12 +15,4 @@ export const useGetDevelopers = () => {
         queryFn: fetchDevelopers,
     });
     return { developers, isLoading };
-};
-
-export const useGetAdmins = () => {
-    const { data: admins, isLoading } = useQuery({
-        queryKey: ["admins"],
-        queryFn: fetchAdmins,
-    });
-    return { admins, isLoading };
 };
